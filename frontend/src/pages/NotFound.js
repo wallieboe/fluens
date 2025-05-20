6e8878ddd16b4d12d3af747bd9b5b0ac8dbef608
wallieboe <115807545+wallieboe@users.noreply.github.com>
2025-05-20 16:40:36 +0200

    fix: use Link on 404 page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-5xl font-bold text-green-500 mb-4">404</h1>
-        <p className="text-lg text-gray-600">Oeps! Pagina niet gevonden.</p>
-        <a href="/" className="mt-6 text-green-600 hover:underline">
-          Ga terug naar Home
-        </a>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-5xl font-bold text-green-500 mb-4">404</h1>
+      <p className="text-lg text-gray-600">Oeps! Pagina niet gevonden.</p>
+      <Link to="/" className="mt-6 text-green-600 hover:underline">
+        Ga terug naar Home
+      </Link>
+    </div>
+  );
+}
   
