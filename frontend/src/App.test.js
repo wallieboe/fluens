7ac3dea7e0f1d6c231522eb76dbe39881eb6bdeb
wallieboe <115807545+wallieboe@users.noreply.github.com>
2025-05-20 16:50:12 +0200

    test: check version text in footer

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders footer with version information', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const footerElement = screen.getByText(/Versie:/i);
+  expect(footerElement).toBeInTheDocument();
 });
